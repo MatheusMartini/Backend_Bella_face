4d563c6e378cf5b7e9e572cfed18d25ace0554ed
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-10-22 00:07:22 -0300

    aligm components

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const rotaProdutos = require('./routes/produtos');
-const rotaPedidos = require('./routes/pedidos');
+const rotaProdutos = require("./routes/produtos");
+const rotaPedidos = require("./routes/pedidos");
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false})); // seta apenas dados simples
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false })); // seta apenas dados simples
 app.use(bodyParser.json());
 
-app.use('/produtos', rotaProdutos)
-app.use('/pedidos', rotaPedidos)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requrested-With, Content-Type, Accept, Authorization"
+  );
 
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).send({});
+  }
+  next();
+});
+
+app.use("/produtos", rotaProdutos);
+app.use("/pedidos", rotaPedidos);
 
 // rota nao definida
-app.use((req, res, next) =>{
-    const erro = new Error('rota nao encontrada');
-    erro.status = 404;
-    next(erro);
+app.use((req, res, next) => {
+  const erro = new Error("rota nao encontrada");
+  erro.status = 404;
+  next(erro);
 });
 // mensagem de erro de rota nao encontrada
-app.use((error, req, res, next)=>{
-    res.status(error.status||500);
-    return res.send({
-        mensagem: error.message
-    });
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    mensagem: error.message,
+  });
 });
 
 module.exports = app;
--- routes/produtos.js
+++ routes/produtos.js
@@ -1,58 +1,55 @@
-const express = require('express');
-const { routes } = require('../app');
+const express = require("express");
+const { routes } = require("../app");
 const router = express.Router();
 
 // return products
-router.get('/',(req, res, next)=>{
-    res.status(200).send({
-        mensagem: 'retorna produtos'
-    });
+router.get("/", (req, res, next) => {
+  res.status(200).send({
+    mensagem: "retorna produtos",
+  });
 });
 
-//insert product post 
-router.post('/',(req, res, next)=>{
+//insert product post
+router.post("/", (req, res, next) => {
+  const produto = {
+    nome: req.body.nome,
+    preco: req.body.preco,
+  };
 
-    const produto = {
-        nome: req.body.nome,
-        preco: req.body.preco
-    };
-
-    res.status(200).send({
-        mensagem: 'criar produtos',
-        produtoCriado: produto
-    });
+  res.status(200).send({
+    mensagem: "criar produtos",
+    produtoCriado: produto,
+  });
 });
 
 //get product by id
-router.get('/:id_produto',(req, res, next)=>{
-    const id = req.params.id_produto
+router.get("/:id_produto", (req, res, next) => {
+  const id = req.params.id_produto;
 
-    if(id == 'especial' ){
-        res.status(200).send({
-            mensagem: 'detalhes do produto especial',
-            id_produto:id
-        });
-    }else{
-        res.status(200).send({
-            mensagem: 'detalhes do produto',
-        });
-    }
-
-});
- 
-// alter product
-router.patch('/',(req, res, next)=>{
+  if (id == "especial") {
     res.status(200).send({
-        mensagem: 'alterar produtos'
+      mensagem: "detalhes do produto especial",
+      id_produto: id,
     });
+  } else {
+    res.status(200).send({
+      mensagem: "detalhes do produto",
+    });
+  }
+});
+
+// alter product
+router.patch("/", (req, res, next) => {
+  res.status(200).send({
+    mensagem: "alterar produtos",
+  });
 });
 
 //delete product
-router.delete('/',(req, res, next)=>{
-    res.status(200).send({
-        mensagem: 'deletar produto'
-    });
+router.delete("/", (req, res, next) => {
+  res.status(200).send({
+    mensagem: "deletar produto",
+  });
 });
-
 
 module.exports = router;
