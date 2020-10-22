3a1413c933d760826c0927880ec1239be4de07cf
Matheus Martini <matheus.martini@rede.ulbra.br>
2020-10-21 23:33:50 -0300

    app

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const rotaProdutos = require('./routes/produtos');
+const rotaPedidos = require('./routes/pedidos');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false})); // seta apenas dados simples
+app.use(bodyParser.json());
+
+app.use('/produtos', rotaProdutos)
+app.use('/pedidos', rotaPedidos)
+
+
+// rota nao definida
+app.use((req, res, next) =>{
+    const erro = new Error('rota nao encontrada');
+    erro.status = 404;
+    next(erro);
+});
+// mensagem de erro de rota nao encontrada
+app.use((error, req, res, next)=>{
+    res.status(error.status||500);
+    return res.send({
+        mensagem: error.message
+    });
+});
+
+module.exports = app;
