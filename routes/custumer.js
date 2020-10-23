52aad6ad125e7a384ff899f5ce99b3e1d571e853
Matheus Martini <matheus.martini@rede.ulbra.br>
2020-10-23 19:05:33 -0300

    custumer

--- routes/custumer.js
+++ routes/custumer.js
@@ -1,79 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const mysql = require("../mysql").pool;
 const { response } = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
+const custumerController = require("../controllers/custumerController");
 
 // add custumer
-router.post("/cadastro",(req, res, next) =>{
-    mysql.getConnection((err, conn) => {
-        if(err){return res.status(500).send({ error: error})} 
-        conn.query("SELECT * FROM custumer WHERE login = ?",[req.body.login],(error, results) => { 
-            if(error){return res.status(500).send({ error: error})} 
-            if(results.length > 0){
-                res.status(409).send({ message: "user ja cadastrado"})
-            }else{
-                bcrypt.hash(req.body.password, 10, (errBcrypt, hash) => {
-                    if(errBcrypt) {return res.status(500).send({ error : errBcrypt})}
-                    conn.query(
-                        `INSERT INTO custumer(id_custumer, name, login, password) VALUES (?,?,?,?)`,
-                        [req.body.id_custumer, req.body.name, req.body.login, hash],
-                        (error, results)=>{
-                            conn.release();
-                            if(error){return res.status(500).send({ error : error})}
-                            const response = {
-                                message: "user created!",
-                                user:{
-                                    id: req.body.id_custumer,
-                                    name: req.body.name,
-                                    login: req.body.login,
-                                    password: "your pass",
-                                }
-                            }
-                            return res.status(201).send(response);
-                        })
-                    });
-            }
-        })
-    });
-})
+router.post("/cadastro", custumerController.createUser)
 
-router.post("/login",(req, res, next) =>{
-    mysql.getConnection((error, conn) => {
-        if(error) {return res.status(500).send({ error: error})} 
-        const query = "SELECT * FROM custumer WHERE login = ?";
-
-        conn.query(query,[req.body.login],(error, results, fields) => {
-            conn.release();
-            if(error) {return res.status(500).send({ error: error})} 
-            if(results.length < 1){
-                return res.status(401).send({ message: "falha na autenticação"})
-            }
-            bcrypt.compare(req.body.password, results[0].password, (err, result) => {
-                if(err){
-                    return res.status(401).send({ message: "falha na autenticação"});
-                }
-                if(result){
-                    const token = jwt.sign({
-                        id_custumer: results[0].id_custumer,
-                        login:results[0].login
-                    },
-                    process.env.JWT_KEY,
-                    {
-                        expiresIn: "2h"
-                    });
-
-                    return res.status(200).send({ 
-                        message: "autenticado com sucesso",
-                        token: token
-                    });
-                }
-                return res.status(401).send({ message: "falha na autenticação"});
-            });
-        });
-    });
-});
+router.post("/login", custumerController.verificationUser);
 
 module.exports = router;
