77167c6e29c79b360d537c803b23e2f595b08d3e
Matheus Martini <matheus.martini@rede.ulbra.br>
2020-10-23 18:34:49 -0300

    usuario e validação

--- routes/custumer.js
+++ routes/custumer.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const mysql = require("../mysql").pool;
-const bcrypt = require("bcrypt");
 const { response } = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+
+// add custumer
 router.post("/cadastro",(req, res, next) =>{
     mysql.getConnection((err, conn) => {
         if(err){return res.status(500).send({ error: error})} 
@@ -36,5 +39,41 @@ router.post("/cadastro",(req, res, next) =>{
         })
     });
 })
+
+router.post("/login",(req, res, next) =>{
+    mysql.getConnection((error, conn) => {
+        if(error) {return res.status(500).send({ error: error})} 
+        const query = "SELECT * FROM custumer WHERE login = ?";
+
+        conn.query(query,[req.body.login],(error, results, fields) => {
+            conn.release();
+            if(error) {return res.status(500).send({ error: error})} 
+            if(results.length < 1){
+                return res.status(401).send({ message: "falha na autenticação"})
+            }
+            bcrypt.compare(req.body.password, results[0].password, (err, result) => {
+                if(err){
+                    return res.status(401).send({ message: "falha na autenticação"});
+                }
+                if(result){
+                    const token = jwt.sign({
+                        id_custumer: results[0].id_custumer,
+                        login:results[0].login
+                    },
+                    process.env.JWT_KEY,
+                    {
+                        expiresIn: "2h"
+                    });
+
+                    return res.status(200).send({ 
+                        message: "autenticado com sucesso",
+                        token: token
+                    });
+                }
+                return res.status(401).send({ message: "falha na autenticação"});
+            });
+        });
+    });
+});
 
 module.exports = router;
