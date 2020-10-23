4843641ad1cac3c377a9aeb641d685e604361eb6
Matheus Martini <matheus.martini@rede.ulbra.br>
2020-10-23 18:50:53 -0300

    orderController

--- routes/order.js
+++ routes/order.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const mysql = require("../mysql").pool;
 
-const orderController = require("../controllers/order.controller")
+const orderController = require("../controllers/orderController")
 
 // return get pedidos
 router.get("/",orderController.getOrder );
-
 //insert pedidos post
 router.post("/",orderController.postOrder);
-
 //get pedidos by id
 router.get("/:order_id", orderController.getOrderById);
-
 // alter quantidade by id
 router.patch("/",orderController.alterQuantityById );
-
 //delete pedido
 router.delete("/", orderController.delete);
 
